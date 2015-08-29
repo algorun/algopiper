40754ad6b19272a74f71053b3364462ea1812281
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-08-29 18:10:38 -0400

    bug fix for nodes sharing the same base code

--- nodes/superADAM/sdds.js
+++ nodes/superADAM/sdds.js
@@ -3,20 +3,57 @@ module.exports = function(RED) {
     var fs = require("fs-extra");
     var os = require("os");
     var request = require('request');
-    var node;
-    var algo_manager;
-    var docker_image;
-    var docker_server = "";
-    var filename;
-    var log_file_path;
-    var module_msg;
-    var log;
     
     function sendDebug(msg) {
         RED.comms.publish("OUTPUT", msg);
     }
     
-    function deploy_container(input_data){
+    function ModuleNode(config) {
+        var node;
+        var algo_manager;
+        var docker_image;
+        var docker_server = "";
+        var filename;
+        var log_file_path;
+        var module_msg;
+        var log;
+        RED.nodes.createNode(this, config);
+        node = this;
+        algo_manager = config.algomanager;
+        docker_image = config.docker_image;
+        log_file_path = config.log_file_path;
+        log = config.log;
+        
+        filename = 'workflow-log/' + node.id + '.json';
+        
+        node.status({fill:"yellow", shape:"dot", text:"connecting .."});
+        deploy_container();
+        
+        
+        this.on('input', function(msg) {
+            module_msg = msg;
+            var input_data = '';
+            try{
+                var json = JSON.parse(msg.payload);
+                input_data = msg.payload.trim();
+            }catch(e) {
+                msg.payload = e.message;
+                node.send(msg);
+                node.status({fill:"red",shape:"dot",text:"error parsing input.."});
+                return;
+            }
+            request(docker_server + '/v1/status',
+                       function(error, response, body){
+                            if(!error && response.statusCode == 200) {
+                                // backend node is running is running
+                                algo_run(input_data);
+                            } else {
+                                // backend node needs to be deployed again
+                                deploy_container(input_data);
+                            }
+            });
+        });
+        function deploy_container(input_data){
         // check algomanager status
         request(algo_manager + '/api/v1/status', function (error, response, body) {
             if (!error && response.statusCode == 200) {
@@ -49,7 +86,7 @@ module.exports = function(RED) {
         });
         
     }
-    function algo_run(input_data){
+        function algo_run(input_data){
         node.status({fill:"blue",shape:"ring",text:"computing .."});
         request.post(
             docker_server + '/v1/run',
@@ -91,44 +128,6 @@ module.exports = function(RED) {
                     }
                 });
     }
-    
-    function ModuleNode(config) {
-        RED.nodes.createNode(this, config);
-        node = this;
-        algo_manager = config.algomanager;
-        docker_image = config.docker_image;
-        log_file_path = config.log_file_path;
-        log = config.log;
-        
-        filename = 'workflow-log/' + node.id + '.json';
-        
-        node.status({fill:"yellow", shape:"dot", text:"connecting .."});
-        deploy_container();
-        
-        
-        this.on('input', function(msg) {
-            module_msg = msg;
-            var input_data = '';
-            try{
-                var json = JSON.parse(msg.payload);
-                input_data = msg.payload.trim();
-            }catch(e) {
-                msg.payload = e.message;
-                node.send(msg);
-                node.status({fill:"red",shape:"dot",text:"error parsing input.."});
-                return;
-            }
-            request(docker_server + '/v1/status',
-                       function(error, response, body){
-                            if(!error && response.statusCode == 200) {
-                                // backend node is running is running
-                                algo_run(input_data);
-                            } else {
-                                // backend node needs to be deployed again
-                                deploy_container(input_data);
-                            }
-            });
-        });
     }
     RED.nodes.registerType('SDDS', ModuleNode);
 }
