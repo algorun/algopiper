c356ba631666341ba0470890f090f93ceae7f063
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-09-10 14:41:30 -0400

    update to bnreduction to latest algomanager update

--- nodes/superADAM/bnreduction.js
+++ nodes/superADAM/bnreduction.js
@@ -4,32 +4,24 @@ module.exports = function(RED) {
     var os = require("os");
     var request = require('request');
     var sleep = require('sleep');
+    var settings = require('./algomanager_settings.js');
     
     function sendDebug(msg) {
         RED.comms.publish("OUTPUT", msg);
     }
     
     function ModuleNode(config) {
-        var node;
-        var algo_manager;
-        var docker_image;
-        var docker_server = "";
-        var filename;
-        var log_file_path;
-        var module_msg;
-        var log;
+        var algomanager = settings.algomanager;
+        var log_file_path = settings.log_file_path;
         RED.nodes.createNode(this, config);
-        node = this;
-        algo_manager = config.algomanager;
-        docker_image = config.docker_image;
-        log_file_path = config.log_file_path;
-        log = config.log;
+        var node = this;
+        var docker_image = config.docker_image;;
+        var docker_server = "";
+        var log = config.log;
+        var filename = 'workflow-log/' + node.id + '.json';;
+        var module_msg;
         
-        filename = 'workflow-log/' + node.id + '.json';
-        
-        node.status({fill:"yellow", shape:"dot", text:"connecting .."});
-        deploy_container();
-        
+        node.status({fill:"green", shape:"dot", text:"ready"});        
         
         this.on('input', function(msg) {
             module_msg = msg;
@@ -40,96 +32,72 @@ module.exports = function(RED) {
             }catch(e) {
                 msg.payload = e.message;
                 node.send(msg);
-                node.status({fill:"red",shape:"dot",text:"error parsing input.."});
+                node.status({fill:"red",shape:"dot",text:"error parsing input!"});
                 return;
             }
-            request(docker_server + '/v1/status',
-                       function(error, response, body){
-                            if(!error && response.statusCode == 200) {
-                                // backend node is running is running
-                                algo_run(input_data);
-                            } else {
-                                // backend node needs to be deployed again
-                                deploy_container(input_data);
-                            }
-            });
-        });
-        function deploy_container(input_data){
-        // check algomanager status
-        request(algo_manager, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                node.status({fill:"yellow", shape:"dot", text:"deploying .."});
-                
-                // now algomanager is running. deploy docker image
-                request.post(algo_manager + '/api/v1/deploy',
-                       { form: { image: docker_image, node_id: node.id } },
-                       function (error, response, body) {
+            node.status({fill:"yellow",shape:"ring",text:"initializing.."});
+            // get endpoint from algomanager
+            request.post(algomanager + '/api/v1/deploy', 
+                         { form: { image: docker_image, node_id: node.id } },
+                        function(error, response, body){        
                             if (!error && response.statusCode == 200) {
                                 var deploy_result = JSON.parse(body);
-                                if(deploy_result["status"] === "success") {
-                                    node.status({fill:"green",shape:"dot",text:"ready .."});
-                                    docker_server = deploy_result["endpoint"];
-                                    if(input_data && input_data !== ''){
-                                        sleep.sleep(2);
-                                        algo_run(input_data);
-                                    }
+                                if(deploy_result['status'] === 'success'){
+                                    var module_server = deploy_result['endpoint'];
+                                    // run computation
+                                    algo_run(module_server, input_data);
                                 } else {
-                                    node.status({fill:"red",shape:"dot",text:body});
-                                    node.status({fill:"red",shape:"dot",text:JSON.stringify(deploy_result["error_message"])});
+                                    console.error(deploy_result['error_message']);
+                                    node.status({fill:"red",shape:"dot",text:'docker deploy error!'});    
                                 }
                             } else {
                                 console.error(error);
-                                node.status({fill:"red",shape:"dot",text:'algomanager server error'});
+                                node.status({fill:"red",shape:"dot",text:'algomanager server error!'});
                             }
-                });
-            } else {
-                node.status({fill:"red", shape:"dot", text:"cannot connect to algomanager"});
-            }
+                        });
         });
-        
-    }
-        function algo_run(input_data){
-        node.status({fill:"blue",shape:"ring",text:"computing .."});
-        request.post(
-            docker_server + '/v1/run',
-            { form: { input: input_data } },
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    module_msg.payload = body;
-                    
-                    if(log !== "No"){
-                        // write data to a file
-                        fs.writeFile(filename, body, "binary", function (err) {
-                            if (err) {
-                                if ((err.code === "ENOENT")) {
-                                    fs.ensureFile(filename, function (err) {
-                                        if (err) { node.error(RED._("file.errors.createfail",{error:err.toString()}),module_msg); }
-                                        else {
-                                            fs.writeFile(filename, body, "binary", function (err) {
-                                                if (err) { node.error(RED._("file.errors.writefail",{error:err.toString()}),module_msg); }
-                                            });
-                                        }
-                                    });
-                                }
-                                else { node.error(RED._("file.errors.writefail",{error:err.toString()}),module_msg); }
-                            }
-                            else if (RED.settings.verbose) { node.log(RED._("file.status.wrotefile",{file:filename})); }
-                        });
+        function algo_run(module_server, input_data){
+            node.status({fill:"blue",shape:"ring",text:"computing.."});
+            request.post(
+                module_server + '/v1/run',
+                { form: { input: input_data } },
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        module_msg.payload = body;
                         
-                        require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-                            var file_path = log_file_path + '/' + filename;
-                            sendDebug({id:node.id,name:"BNReduction LOG",topic:"computation result",msg:file_path,_path:module_msg._path});
-                        });
-                    }
-                    node.send(module_msg);
-                    node.status({fill:"green",shape:"dot",text:"ready .."});
-                    } else {
-                        module_msg.payload = error;
+                        if(log !== "No"){
+                            // write data to a file
+                            fs.writeFile(filename, body, "binary", function (err) {
+                                if (err) {
+                                    if ((err.code === "ENOENT")) {
+                                        fs.ensureFile(filename, function (err) {
+                                            if (err) { node.error(RED._("file.errors.createfail",{error:err.toString()}),module_msg); }
+                                            else {
+                                                fs.writeFile(filename, body, "binary", function (err) {
+                                                    if (err) { node.error(RED._("file.errors.writefail",{error:err.toString()}),module_msg); }
+                                                });
+                                            }
+                                        });
+                                    }
+                                    else { node.error(RED._("file.errors.writefail",{error:err.toString()}),module_msg); }
+                                }
+                                else if (RED.settings.verbose) { node.log(RED._("file.status.wrotefile",{file:filename})); }
+                            });
+                            
+                            require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+                                var file_path = log_file_path + '/' + filename;
+                                sendDebug({id:node.id,name:"BNReduction LOG",topic:"computation result",msg:file_path,_path:module_msg._path});
+                            });
+                        }
                         node.send(module_msg);
-                        node.status({fill:"red",shape:"dot",text:JSON.stringify(error)});
-                    }
-                });
-    }
+                        node.status({fill:"green",shape:"dot",text:"ready .."});
+                        } else {
+                            module_msg.payload = error;
+                            node.send(module_msg);
+                            node.status({fill:"red",shape:"dot",text:JSON.stringify(error)});
+                        }
+                    });
+        }
     }
     RED.nodes.registerType('BNReduction', ModuleNode);
 }
