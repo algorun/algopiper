3f62fa62b9fcae382013ed7be31b923e408a37b6
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-09-14 02:05:43 -0400

    change server settings

--- nodes/AlgoPiper/algomanager_settings.js
+++ nodes/AlgoPiper/algomanager_settings.js
@@ -1,8 +1,8 @@
 // Define gloabl settings for all nodes to use
 module.exports = {
-    algomanager: 'http://localhost:8764'
-    // algomanager: 'http://algomanager.algorun.org'
+    //algomanager: 'http://localhost:8764'
+    algomanager: 'http://algomanager.algorun.org'
 ,
-    log_file_path: 'http://localhost:1880'
-    // log_file_path: 'http://algopiper.algorun.org'
+    //log_file_path: 'http://localhost:1880'
+    log_file_path: 'http://algopiper.algorun.org'
 }
