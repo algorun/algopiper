14f4038544b115c241fdb3023a60497338f434ae
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-09-15 19:46:51 -0400

    first attempt to re-brand

--- red/api/theme.js
+++ red/api/theme.js
@@ -22,11 +22,11 @@ var clone = require("clone");
 
 var defaultContext = {
     page: {
-        title: "Node-RED",
+        title: "AlgoPiper",
         favicon: "favicon.ico"
     },
     header: {
-        title: "Node-RED",
+        title: "AlgoPiper",
         image: "red/images/node-red.png"
     },
     asset: {
