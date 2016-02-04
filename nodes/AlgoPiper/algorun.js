ce919959c8aacbae293cf35c70f777b660aea6f7
Abdelrahman <abdelrahman.hosny@hotmail.com>
2016-02-03 22:51:59 -0500

    dynamically list available nodes on algomanager

--- nodes/AlgoPiper/algorun.js
+++ nodes/AlgoPiper/algorun.js
@@ -15,42 +15,35 @@ module.exports = function(RED) {
         var node = this;
         var docker_image = config.docker_image;
         var docker_server = "";
-        var log = config.log;
-        var filename = 'workflow-log/' + node.id + '.json';;
+        var log_file = 'workflow-log/' + node.id + '.json';
         var module_msg;
         
         node.status({fill:"green", shape:"dot", text:"ready"});        
         
-        
         this.on('input', function(msg) {
             module_msg = msg;
-            var input_data = '';
-            try{
-                var json = JSON.parse(msg.payload);
-                input_data = msg.payload.trim();image_name
-            }catch(e) {
-                msg.payload = e.message;
-                node.send(msg);
-                node.status({fill:"red",shape:"dot",text:"error parsing input!"});
-                return;
-            }
-
+            var input_data = msg.payload;
+            
             node.status({fill:"yellow",shape:"ring",text:"initializing.."});
+            
+            
             // get endpoint from algomanager
-            request.get(algomanager + '/api/v1/get_url?image='+docker_image, 
-                        function(error, response, body){        
+            request.post(algomanager + '/api/v1/deploy', {form: {'docker_image': docker_image, node_id: node.id}}, 
+                         function(error, response, body){        
                             if (!error && response.statusCode == 200) {
                                 var deploy_result = JSON.parse(body);
-                                if(deploy_result['status'] != ''){
+                                if(deploy_result['status'] === 'success'){
                                     var module_server = deploy_result['endpoint'];
                                     // run computation
                                     algo_run(module_server, input_data);
                                 } else {
                                     console.error(deploy_result['error_message']);
-                                    node.status({fill:"red",shape:"dot",text:'docker deploy error!'});    
+                                    node.status({fill:"red",shape:"dot",text:'docker container deployment error!'});    
                                 }
                             } else {
                                 console.error(error);
+                                console.error(response);
+                                console.error(body);
                                 node.status({fill:"red",shape:"dot",text:'algomanager server error!'});
                             }
                         });
@@ -64,32 +57,30 @@ module.exports = function(RED) {
                     if (!error && response.statusCode == 200) {
                         module_msg.payload = body;
                         
-                        if(log !== "No"){
-                            // write data to a file
-                            fs.writeFile(filename, body, "binary", function (err) {
-                                if (err) {
-                                    if ((err.code === "ENOENT")) {
-                                        fs.ensureFile(filename, function (err) {
-                                            if (err) { node.error(RED._("file.errors.createfail",{error:err.toString()}),module_msg); }
-                                            else {
-                                                fs.writeFile(filename, body, "binary", function (err) {
-                                                    if (err) { node.error(RED._("file.errors.writefail",{error:err.toString()}),module_msg); }
-                                                });
-                                            }
-                                        });
-                                    }
-                                    else { node.error(RED._("file.errors.writefail",{error:err.toString()}),module_msg); }
+                        // write data to a file
+                        fs.writeFile(log_file, body, "binary", function (err) {
+                            if (err) {
+                                if ((err.code === "ENOENT")) {
+                                    fs.ensureFile(log_file, function (err) {
+                                        if (err) { node.error(RED._("file.errors.createfail",{error:err.toString()}),module_msg); }
+                                        else {
+                                            fs.writeFile(log_file, body, "binary", function (err) {
+                                                if (err) { node.error(RED._("file.errors.writefail",{error:err.toString()}),module_msg); }
+                                            });
+                                        }
+                                    });
                                 }
-                                else if (RED.settings.verbose) { node.log(RED._("file.status.wrotefile",{file:filename})); }
-                            });
+                                else { node.error(RED._("file.errors.writefail",{error:err.toString()}),module_msg); }
+                            }
+                            else if (RED.settings.verbose) { node.log(RED._("file.status.wrotefile",{file:log_file})); }
+                        });
                             
-                            require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-                                var file_path = '/' + filename;
-                                sendDebug({id:node.id,name:"Custom LOG",topic:"computation result",msg:file_path,_path:module_msg._path});
-                            });
-                        }
+                        var file_path = "/" + log_file;
+                        sendDebug({id:node.id,name:"Custom LOG",topic:"computation result", msg:file_path, _path:module_msg._path});
+                        
                         node.send(module_msg);
                         node.status({fill:"green",shape:"dot",text:"ready .."});
+                        
                         } else {
                             module_msg.payload = error;
                             node.send(module_msg);
